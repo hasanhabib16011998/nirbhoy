b539321bed382d22796ec9a432ccded50f8c5ffc
hasanhabib16011998 <hasan16habib@gmail.com>
2026-02-02 14:40:15 +0600

    setup routes

--- social-media/src/App.tsx
+++ social-media/src/App.tsx
@@ -1,8 +1,27 @@
+import AuthLayout from './_auth/AuthLayout';
+import SignInForm from './_auth/forms/SignInForm';
+import SignUpForm from './_auth/forms/SignUpForm';
+import { Home } from './_root/pages';
+import RootLayout from './_root/RootLayout';
 import './globals.css';
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div className='text-2xl underline font-bold'>App</div>
+    <main className='flex h-screen'>
+        <Routes>
+          <Route element={<AuthLayout/>}>
+            {/* public routes */}
+            <Route path='/sign-in' element={<SignInForm/>}/>
+            <Route path='/sign-up' element={<SignUpForm/>}/>
+          </Route>
+
+            {/* private routes */}
+            <Route element={<RootLayout/>}>
+              <Route index element={<Home/>}/>
+            </Route>
+        </Routes>
+    </main>
   )
 }
 
